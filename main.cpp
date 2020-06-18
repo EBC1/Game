0ec0116355799f9071ecc041da22f349c41d842d
Erich Lohrmann <Erich.Lohrmann@gtri.gatech.edu>
2020-06-18 15:04:48 -0400

    Made changes to make it more readable

--- main.cpp
+++ main.cpp
@@ -4,38 +4,44 @@ using namespace std;
 
 
 int main()
- {
-   int hidNum = 7;
-   int guess;
+{
+    int hidNum = 7;
+    int guess;
     int limit = 5;
     int guessCount = 0;
     bool outOfGuesses = false;
 
 
 
-   while(hidNum != guess){
-    if (guessCount < limit) {  cout << "Enter guess: ";
-        cin >> guess;
-        guessCount++;
-       } else {
-       outOfGuesses = true;
-       }
-
-    if(outOfGuesses){
-            cout << "You Lose!";
-        } else {
-        cout << "You Win!";
-
+    while(hidNum != guess)
+    {
+        if (guessCount < limit) 
+        {
+            cout << "Enter guess: ";
+            cin >> guess;
+            guessCount++;
+        }
+        else
+        {
+            outOfGuesses = true;
         }
 
+        if(outOfGuesses)
+        {
+            cout << "You Lose!";
+        } 
+        else
+        {
+            cout << "You Win!";
+        }
 
-   }
+    }
 
 
 
-  return 0;
+    return 0;
 
-   }
+}
 
 
 
