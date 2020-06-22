6b31b6d1eb122db4ad0cde9a810164cb5519a338
EBC1 <EBC1>
2020-06-22 15:27:47 -0400

    Added more strings

--- SentInput.cpp
+++ SentInput.cpp
@@ -10,14 +10,34 @@ int main()
 char word;
 int woRdlim = 10;
 
-string wordstr( word );
+string wordstr;
+string wordstr2;
+string wordstr3;
+string wordstr4;
+string wordstr5;
+string wordstr6;
+string wordstr7;
+string wordstr8;
+string wordstr9;
+string wordstrX;
+
 
 cout<< wordstr << endl;
 
 cout << "Just give me a sentence of up to 10 words. The words can be as long as you want" << endl;
-cin >> word;
+cin >> wordstr;
+cin >> wordstr2;
+cin >> wordstr3;
+cin >> wordstr4;
+cin >> wordstr5;
+cin >> wordstr6;
+cin >> wordstr7;
+cin >> wordstr8;
+cin >> wordstr9;
+cin >> wordstrX;
+cout << wordstr << endl;
 
-if  (word > woRdlim)
+if  (wordstr > woRdlim)
 {
 cout << "Too many words..." << endl;
 }
