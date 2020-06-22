1f54326ba84aa40a14dc0af4e4f7ad90ed11f5e5
EBC1 <EBC1>
2020-06-22 13:57:07 -0400

    Erased alot of junk but i keep getting a path issue whenever I try to build/compile

--- SentInput.cpp
+++ SentInput.cpp
@@ -1,38 +1,29 @@
 #include <iostream>
-using namespace std;
-
-
-int main();
-{
-#include <iostream>
 #include <sstring>
 using namespace std;
 
-
-int main();
+int main()
 {
 
-int woRdlim = s10
 
-string sEntence = int woRd
+
+{
+
+int woRdlim = 10;
+
 
 
 cout<< "Just give me a sentence of up to 10 words. The words can be as long as you want" < endl;
+cin << "Go ahead and type:" "." << endl;
 
-cin << "Go ahead and type:" woRd_woRd_woRd_woRd_woRd_woRd_ woRd_woRd_woRd_woRd"." << endl;
-for(woRd = 0; woRd < woRdlim; )
-if  (woRd > woRdlim) {
+if  (woRd > woRdlim)
+{
 cout << "Too many words..." << endl;
 }
 
 return 0;
 }
 
-// Should I keep the code below
-
-    for char = 1
-cout<< "Just give me a sentence of up to 10 words. The words can be as long as you want" << endl;
-for (int numb = maxEl; numb >= 0; numb-- )
 
 
 
