fb1d013d9ac05a01beeaea860dc4296a0576277c
EBC1 <EBC1>
2020-06-22 10:45:06 -0400

    Working  on input histogram. Yes i know its a bit messy.

---
+++ SentInput.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+
+int main();
+{
+MAXWORD length = 10
+char woRdS =
+double alphaB [26]
+
+int aA = (int a , int A)
+bB bB
+cC
+dD
+eE
+fF
+gG
+hH
+iI
+jJ
+kK
+lL
+mM
+nN
+oP
+qQ
+rR
+sS
+tT
+uU
+vV
+wW
+xX
+yY
+zZ
+
+    for char = 1
+cout<< "Just give me a sentence of up to 10 words. The words can be as long as you want" << endl;
+for (int numb = maxEl; numb >= 0; numb-- )
+
+for (int aFb = )
+
+if MAXWORDLENGTH = 10  {cout << "Too many words. Try to stay under 10 words or this wont work too well."
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
