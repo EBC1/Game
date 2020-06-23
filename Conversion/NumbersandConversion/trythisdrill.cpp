139c93cd2caace7e206d961215aa66096a8d8777
EBC1 <EBC1>
2020-06-23 15:42:40 -0400

    Conversion

---
+++ Conversion/NumbersandConversion/trythisdrill.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+        double d = 0;
+        while (cin>>d)
+
+        {
+        int i = d ;
+        char c = i;
+        int i2 = c;
+        cout << " d is equal to " << d << endl;
+        cout << " i is equal to " << i << endl;
+        cout << " i2 is equal to " << i2 << endl;
+
+
+
+
+
+        }
+
+
+
+        int a = 20000;
+        char c = a;
+        int b = c;
+        if (a != b)
+        cout << "Nope!: " << a << " Not equal to " << b << endl;
+        else
+        cout << "Wow! We have too large of a number" << endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Conversion/PPPCPPdrills/c++drills.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+int main()
+
+    {
+    double age;
+    string first_name;
+    string answEr1
+    string haveyouseen;
+    string seenyes;
+    string seenno;
+
+    cout << "Whats your name? Preferably your first name." << endl;
+    cin<< first_name << endl;
+    cout << "Hi, " << first_name << ". My name is 17143787943564093569643975795640956430956395624905643976651964065-57. Nice to meet you. Say, how are you doing?" << endl;
+    cin << answEr1 << endl;
+    cout << "Ok, anyways that doesnt matter. What does matter is if youve seen my pet dog spot." /.n
+    << " Well have you seen him?"
+    cin << haveyouseen
+
+    "Next year youll be" << age + 1 << ". Thats crazy"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
