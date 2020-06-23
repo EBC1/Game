0a60f3a85e95c3c3caaa3919403895fe1ae81f11
EBC1 <EBC1>
2020-06-23 15:57:12 -0400

    Started drill but only halfway. Does compile but isnt finished.

--- Conversion/PPPCPPdrills/c++drills.cpp
+++ Conversion/PPPCPPdrills/c++drills.cpp
@@ -6,20 +6,23 @@ int main()
     {
     double age;
     string first_name;
-    string answEr1
+    string answEr1;
     string haveyouseen;
     string seenyes;
     string seenno;
 
     cout << "Whats your name? Preferably your first name." << endl;
-    cin<< first_name << endl;
+    cin>> first_name;
     cout << "Hi, " << first_name << ". My name is 17143787943564093569643975795640956430956395624905643976651964065-57. Nice to meet you. Say, how are you doing?" << endl;
-    cin << answEr1 << endl;
-    cout << "Ok, anyways that doesnt matter. What does matter is if youve seen my pet dog spot." /.n
-    << " Well have you seen him?"
-    cin << haveyouseen
+    cin >> answEr1;
+    cout << "Ok, anyways that doesnt matter. What does matter is if youve seen my pet dog spot." << endl;
+    cout << " Well have you seen him?" << endl;
 
-    "Next year youll be" << age + 1 << ". Thats crazy"
+
+
+    cout << "How old are you?" << endl;
+    cin >> age;
+    cout << "Next year youll be" << age + 1 << ". Thats crazy" << endl;
 
 
 
