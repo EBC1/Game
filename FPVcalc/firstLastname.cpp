b2a6fe8df22e363e75fa1b24a5073e9f7f0b8278
EBC1 <EBC1>
2020-06-23 13:31:59 -0400

    FPVcalc/firstLastname.cpp

---
+++ FPVcalc/firstLastname.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+ int main()
+
+{
+
+    string fullName;
+    cout << "Enter first and last name (Followed by enter key)"
+    string firstName;
+    string secondName;
+    cin >> firstName >> secondName;
+    string fullName=firstName + '' +secondName;
+    cout << "Hello, " << fullName << endl;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
