bdcb5cc5294d902c0bba11daf8046ec8a4d25fb8
EBC1 <EBC1>
2020-06-23 11:51:08 -0400

    COnca puts two strings together

--- firstlastsqrt.cpp
+++ firstlastsqrt.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 int main()
@@ -8,8 +9,12 @@ int main()
     cin >> numB;
     cout << "Number is " << numB << endl;
     cout << "The number plus one is "  << numB + 1 << endl;
-    cout << "Three times this number is " << 3*numB << endl;
-
+    cout << " Three times this number is " << 3*numB << endl
+         << " Double this number is " << numB + numB << endl
+         << " This number squared is " << numB * numB << endl
+         << " Half of this number is " << numB/2 << endl
+         << "The square root of this number is " << sqrt(numB)
+         << endl;
 
 
 
