b081648244b0dd0104433ff498ee043cd6fd3107
EBC1 <EBC1>
2020-06-24 14:45:11 -0400

    Exercise 11 is somewhat simple.. having trouble with getting the math together. I need a way to add up cents and cash. if cents exceeds or equals 1.00 then cash increases by 1. Or I could also increase by 1 then added to cash. Idk. ANy ideas.  c the commit message for your changes. Lines starting

---
+++ exercXI.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+double pennies;
+double nickels;
+double dimes;
+double quarters;
+double hdollar;
+double dollar;
+double
+nickels == 0.05;
+pennies == 0.01;
+dimes == 0.10;
+quarters == 0.25;
+hdollar == 0.50;
+dollar == 1.00;
+
+cout << " Gimme your money, how much you got " << endl;
+cout << " How many pennies do you have " << endl;
+cin >> pennies;
+cout << " nickels? " << endl;
+cin >> nickels;
+cout << "what about dimes?" << endl;
+cin >> dimes;
+cout << "quarters" << endl;
+cin >> quarters;
+cout << " half dollars also ,you look like you carry half dollars...." << endl;
+cin >> hdollar;
+cout << " lastly, how many bucks you got? You got any cash on you right now?" << endl;
+if (cents >= 1.00; dollar++)
+{
+
+}
+
+
+
+
+
+}
