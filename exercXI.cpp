067ebb2d52ff40319bcd8b58f9fb8c2dfa096f51
EBC1 <EBC1>
2020-06-24 15:32:19 -0400

    Complete jhust need to find a better conversion

--- exercXI.cpp
+++ exercXI.cpp
@@ -10,14 +10,10 @@ double dimes;
 double quarters;
 double hdollar;
 double dollar;
-double
-nickels == 0.05;
-pennies == 0.01;
-dimes == 0.10;
-quarters == 0.25;
-hdollar == 0.50;
-dollar == 1.00;
+double cents = pennies + nickels * 5 + dimes * 10 + quarters * 25 + hdollar * 50 + dollar * 100;
 
+double total = cents* 0.01;
+double aboutTotal = round(total);
 cout << " Gimme your money, how much you got " << endl;
 cout << " How many pennies do you have " << endl;
 cin >> pennies;
@@ -30,9 +26,11 @@ cin >> quarters;
 cout << " half dollars also ,you look like you carry half dollars...." << endl;
 cin >> hdollar;
 cout << " lastly, how many bucks you got? You got any cash on you right now?" << endl;
-if (cents >= 1.00; dollar++)
-{
+cin >> dollar;
 
+{
+cout << "so you have $"<< total << endl;
+cout << "Or about $" << aboutTotal << endl;
 }
 
 
