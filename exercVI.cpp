0ff323062536ec82b6f61a51840e17d6b99697db
EBC1 <EBC1>
2020-06-24 12:56:56 -0400

    Exercise 6 wont compile due to an issue with main for some reason. Thast why iv been making new projectsbecuase the way codeblocks works is wierd.  c enter the commit message for your changes. Lines starting

---
+++ exercVI.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+
+    double integerUno;
+    double integerDos;
+    double integerTres;
+
+    cout << "Enter three numbers. One after each enter please." << endl;
+    cin >> integerUno , integerDos, integerTres;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
