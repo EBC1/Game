93b86675cd26c56621494a81aebdf2f025f00f1a
EBC1 <EBC1>
2020-06-24 14:07:23 -0400

    This version works... on to exercise 11

--- exercVI.cpp
+++ exercVI.cpp
@@ -4,16 +4,20 @@ using namespace std;
 
 int main()
     {
-    double integer1;
-    double integer2;
-    double integer3;
+
+
+    cout << "Enter three numbers. (Followed with 'Enter' " <<  endl;
+    double integer1, integer2, integer3;
+    cin >> integer1 >> integer2 >> integer3;
+
     double smallest, middle, largest;
-    cout << "Enter three numbers. One after each enter please." << endl;
-    cin >> integer1 , integer2, integer3;
+
     if (integer1 < integer2)
-    { smallest = integer1;
-      largest = integer2;
+    {
+    smallest = integer1;
+    largest = integer2;
     }
+
     else
     {
     smallest = integer2;
@@ -22,23 +26,24 @@ int main()
 
     if (integer3 <= smallest)
     {
-        middle = smallest;
-        smallest =integer3;
+    middle = smallest;
+    smallest =integer3;
     }
+
     else if (smallest < integer3 && integer3 < largest)
     {
-        middle = integer3;
+    middle = integer3;
     }
     else if (largest <= integer3)
     {
         middle = largest;
-        largest = integer1;
+        largest = integer3;
     }
 
-    cout << smallest << "," << middle << "," << largest << endl;
+    cout << smallest << " , " << middle << " , " << largest << endl;
 
-    return 0;
     }
+
 
 
 
