5f4194815e896d92eee7b3479f7c73a6ad2a5098
lift-orange <edwin.culpepper@gtri.gatech.edu>
2020-06-25 15:45:16 -0400

    main.cpp

--- main.cpp
+++ main.cpp
@@ -4,17 +4,24 @@ using namespace std;
 
 int main()
 {
-int i = 0;
-while (i <= 25)
-    {
-    int letval = 'a' + i;
-    cout << char(letval) << '\t' << int(letval) << endl;
-    ++i ;
 
+    int currency;
+    cout <<  "What currency do you wish to transfer to dollars. (yrn for Yen , BP for British Pounds, eur for Euro)." << endl;
+    cin >> currency;
+    if ('eur' == currency)
+        {
+            cout << "$     "<< currency * 1.12 << endl;
+        }
+    else if ('yen' == currency)
+        {
+            cout << "$ " << currency * 0.0093   << endl;
+        }
+    else ('BP' == currency);
+        {
+            cout << "$ " << currency * 1.24 << endl;
+        }
+}
 
-
-
-    }
 
 
   }
