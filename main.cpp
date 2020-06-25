27d7db35d4ee7989d759f7c3d775200a0ad45c0b
EBC1 <EBC1>
2020-06-25 09:46:17 -0400

    This is git exercise 2. it converts miles to km and vice versa.

--- main.cpp
+++ main.cpp
@@ -1,47 +1,31 @@
 #include <iostream>
-
+#include <math.h>
 using namespace std;
 
-
 int main()
-{
-    int hidNum = 7;
-    int guess;
-    int limit = 5;
-    int guessCount = 0;
-    bool outOfGuesses = false;
 
-
-
-    while(hidNum != guess)
     {
-        if (guessCount < limit) 
+        double measurNumber;
+        char measurAns;
+
+
+
+        cout << "Do you want to measure in the metric system or imperial system. (Enter 'y' for yes  or 'n' for no)" << endl;
+        cin >> measurAns;
+        if ( 'y' == measurAns)
         {
-            cout << "Enter guess: ";
-            cin >> guess;
-            guessCount++;
+        cout << " Good choice, nonone likes to measure like that anyways. But go ahead and enter a number and I'll convert it to Miles(m) just in case you made a mistake. " << endl;
+        cin >> measurNumber;
+        cout << measurNumber << " converted to miles is " << measurNumber / 1.609 << " miles"  << endl;
         }
-        else
+        else if ( 'n' == measurAns)
         {
-            outOfGuesses = true;
+        cout << "Guess we're doing this the imperial way then... I'll go ahead and ask for a number any number incase you want to see it converted to Kilometers. (km) " << endl;
+        cin >> measurNumber;
+        cout << measurNumber << " converted to Kilometers is " << measurNumber * 1.609 << " kilometers" << endl;
         }
-
-        if(outOfGuesses)
-        {
-            cout << "You Lose!";
-        } 
-        else
-        {
-            cout << "You Win!";
-        }
-
-    }
-
-
-
-    return 0;
-
 }
+
 
 
 
