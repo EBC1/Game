8d75af7fe6edf314bae7890192d2618364cc9ca9
lift-orange <edwin.culpepper@gtri.gatech.edu>
2020-06-30 15:01:49 -0400

    This does compile but is WIP

--- main.cpp
+++ main.cpp
@@ -7,15 +7,16 @@ using namespace std;
     int main()
     {
 
-        string input;
-        vector<string>words;
-        vector<string>disliked = {"Broccoli", "Puree", "Cauliflower", "Cabbage"};
+
+        vector <string> words;
+        vector<string> disliked = {"Broccoli", "Puree", "Cauliflower", "Cabbage"};
 
         cout << "Enter a word. Proper puncuation please: " << endl;
-        while (cin>> words)
-            words.push_back(input)
-        if ( words  == disliked)
-        disliked.push_back("Broccoli", "Puree", "Cauliflower", "Cabbage");
+  for (string input; cin >> input;)
+    words.push_back(input);
+
+
+
 
 
         for  (int i = 0; i < words.size(); ++i)
