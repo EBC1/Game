88592f4c4ba7cb63fe5feec99f86bfff1ae95a76
lift-orange <edwin.culpepper@gtri.gatech.edu>
2020-06-30 14:11:30 -0400

    This doesnt compile and is a WIP

--- main.cpp
+++ main.cpp
@@ -2,16 +2,28 @@
 #include <cmath>
 using namespace std;
 
+
 int main()
 {
+
+constexpr double cmtom  {0.01};
+constexpr double intom { 2.54*cmtom};
+constexpr double fttom {12.0*intom};
+const vector<string> allowedunits {}
     cout << "Enter a double value." << endl;
     bool first {true};
     double value {0.0};
     double least{0.0};
     double greatest {0.0};
-    string unit {" "};
+    doble currentmeters
+    string measur;
 
-    while (cin >> value)
+    if ('cm' == unitmeasur)
+
+
+
+
+    while (cin >> value>> unit)
     {
         cout << value;
         if (first == true)
@@ -19,7 +31,7 @@ int main()
             first = false;
             least = value;
             greatest = value;
-            cout << " is the only value inputed so far." << endl;
+            cout << " is the only number inputed so far." << endl;
         }
         else if (value < least)
         {
