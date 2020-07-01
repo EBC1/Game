2b9fee9155c2f57b2818fa266fe19a6b4cfa260d
lift-orange <edwin.culpepper@gtri.gatech.edu>
2020-07-01 15:26:13 -0400

    This doesnt compile check line 38

--- main.cpp
+++ main.cpp
@@ -20,21 +20,57 @@ int main()
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+    string unitMeasur;
+    double userVal;
     cout << "Enter a value." << endl;
     bool first {true};
-    double value {0.0};
+    double value = userVal; {0.0};  //Whats wrong here?
     double least{0.0};
     double greatest {0.0};
-    string legalInput;
+    string legalInput = unitMeasur;
 
-
+    cout << "Enter a number and unit of measurement" << endl;
     while (cin >> value >> legalInput)
     {
+
+        if (unitMeasur == "cm")
+            {
+                cout << userVal * 0.01 << "m"<< endl;
+            }
+        else if (unitMeasur == "in")
+            {
+                cout << userVal* 0.0254<< "m" << endl;
+            }
+        else if (unitMeasur == "ft")
+            {
+                cout << userVal * 0.3048 << "m" << endl;
+            }
+        else if (unitMeasur == "m")
+            {
+                cout << userVal * 100 << "cm " << endl;
+
+                cout << userVal * 39.3701 << "in " << endl;
+
+                cout << userVal* 3.2084 << "ft " << endl;
+            }
+
+
         if (isLegalString(legalInput))
             {
 
 
-                cout << value <<legalInput;
+                cout << userVal <<legalInput;
             if (first == true)
                 {
                     first = false;
@@ -62,8 +98,9 @@ int main()
             cout << "not valid unit of measurement. Instead use 'cm','in','ft', or 'm' " << endl;
             break;
         }
-    }
+
     return 0;
+    }
 }
 
 
---
+++ unitconversion.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+int d;
+
+
+
+
+
+int main()
+{
+
+
+
+
+    string unitMeasur;
+    cout << "Enter a number and unit of measurement" << endl;
+    double userVal;
+    while (cin>> userVal>> unitMeasur)
+    {
+
+        if (unitMeasur == "cm")
+            {
+                cout << userVal * 0.01 << "m"<< endl;
+            }
+        else if (unitMeasur == "in")
+            {
+                cout << userVal* 0.0254<< "m" << endl;
+            }
+        else if (unitMeasur == "ft")
+            {
+                cout << userVal * 0.3048 << "m" << endl;
+            }
+        else if (unitMeasur == "m")
+            {
+                cout << userVal * 100 << "cm " << endl;
+
+                cout << userVal * 39.3701 << "in " << endl;
+
+                cout << userVal * 3.2084 << "ft " << endl;
+            }
+
+
+
+
+return 0;
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
