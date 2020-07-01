dca5f0c5edf97ea04a58b0dbf281f599ee715af9
lift-orange <edwin.culpepper@gtri.gatech.edu>
2020-07-01 14:16:26 -0400

    This does compile but is WIP and needs to have a measurement value converter

--- main.cpp
+++ main.cpp
@@ -2,47 +2,63 @@
 #include <cmath>
 using namespace std;
 
-bool isIllegalString(std::string input)
+bool isLegalString(string input)
 {
 
 
-	return true;
+    if (input == "cm" || input == "in" || input =="m" || input == "ft")
+        return true;
+    else
+        return false;
+
 }
 
 
 
 int main()
 {
-    cout << "Enter a double value." << endl;
+
+
+
+    cout << "Enter a value." << endl;
     bool first {true};
     double value {0.0};
     double least{0.0};
     double greatest {0.0};
-    string unit {" "};
+    string legalInput;
 
-    while (cin >> value)
+    while (cin >> value >> legalInput)
     {
-        cout << value;
-        if (first == true)
-        {
-            first = false;
-            least = value;
-            greatest = value;
-            cout << " is the only value inputed so far." << endl;
-        }
-        else if (value < least)
-        {
-            cout << " this is the smallest number so far" << endl;
-            least = value;
-        }
-        else if (value > greatest)
-        {
-            cout << " this is the largest number by far" << endl;
-            greatest = value;
-        }
+        if (isLegalString(legalInput))
+            {
+
+
+                cout << value <<legalInput;
+            if (first == true)
+                {
+                    first = false;
+                    least = value;
+                    greatest = value;
+                    cout << " is the only value inputed so far." << endl;
+                }
+            else if (value < least)
+                {
+                    cout << " this is the smallest number so far" << endl;
+                    least = value;
+                }
+            else if (value > greatest)
+                {
+                    cout << " this is the largest number by far" << endl;
+                    greatest = value;
+                }
+            else
+                {
+                    cout << '\n';
+                }
+            }
         else
         {
-            cout << '\n';
+        cout << "not valid unit of measurement. Instead use 'cm','in','ft', or 'm' " << endl;
         }
     }
     return 0;
