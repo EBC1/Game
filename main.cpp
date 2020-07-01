ca33458cdedb635059c552fe1c83301d4154816a
lift-orange <edwin.culpepper@gtri.gatech.edu>
2020-07-01 12:38:08 -0400

    This is  a WIP

--- main.cpp
+++ main.cpp
@@ -1,55 +1,94 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
+#include <algorithm>
 using namespace std;
+int d;
+
+const vector<string> allowedUnits = {"m", "in", "cm", "ft",};
+
 
 
 int main()
 {
 
-constexpr double cmtom  {0.01};
-constexpr double intom { 2.54*cmtom};
-constexpr double fttom {12.0*intom};
-const vector<string> allowedunits {}
     cout << "Enter a double value." << endl;
-    bool first {true};
-    double value {0.0};
-    double least{0.0};
-    double greatest {0.0};
-    doble currentmeters
-    string measur;
+    string inputValue;
+    cin >> inputValue;
+    cout << inputValue;
+//    bool first {true};
+//    double value {0.0};
+//    double least{0.0};
+//    double greatest {0.0};
+//    string unitMeasur;
+//    cout << "Enter a number and unit of measurement" << endl;
+//
+//    while (cin>> value>> unitMeasur)
+//    {
+//
+//    if (unitMeasur == "cm")
+//        {
+//        cout << value * 0.01 << "m"<< endl;
+//        }
+//    else if (unitMeasur == "in")
+//    {
+//        cout << value * 0.0254<< "m" << endl;
+//    }
+//    else if (unitMeasur == "ft")
+//     cout << value * 0.3048 << "m" << endl;
+//
 
-    if ('cm' == unitmeasur)
+
+return 0;
 
 
 
 
-    while (cin >> value>> unit)
-    {
-        cout << value;
-        if (first == true)
-        {
-            first = false;
-            least = value;
-            greatest = value;
-            cout << " is the only number inputed so far." << endl;
-        }
-        else if (value < least)
-        {
-            cout << " this is the smallest number so far" << endl;
-            least = value;
-        }
-        else if (value > greatest)
-        {
-            cout << " this is the largest number by far" << endl;
-            greatest = value;
-        }
-        else
-        {
-            cout << '\n';
-        }
+
     }
+
+
+
+
+
+
+
+    /*while (cin >> value >> unitMeasur)
+    {
+
+            if ()
+
+            cout << value;
+            if (first == true)
+            {
+                first = false;
+                least = value;
+                greatest = value;
+                cout << " is the only number inputed so far and the unit of measurement is " << unitMeasur <<endl;
+                cout << "In meters this is " <<
+            }
+            else if (value < least)
+            {
+                cout << " this is the smallest number so far and is in  " << endl;
+                least = value;
+            }
+            else if (value > greatest)
+            {
+                cout << " this is the largest number by far" << endl;
+                greatest = value;
+            }
+            else
+            {
+                cout << '\n';
+            }
+
+    }
+
     return 0;
 }
+*/
+
+
 
 
 
