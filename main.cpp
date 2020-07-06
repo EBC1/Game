0edaabf95326a89be4178842e9de018bf24e8fcd
Your Name <ebculpepper246810@gmail.com>
2020-07-06 12:12:47 -0400

    Better version, QOL changes to be honest just more readable overal.

--- main.cpp
+++ main.cpp
@@ -9,30 +9,35 @@ using namespace std;
 
 int main()
 {
-    int riceGoal1 = 1000;
-    int riceGoal2 = 1000000;
-    int riceGoal3 = 1000000000;
-    int riceAmount =1;
-    int squareNumb = 1;
+    vector<int> riceGoals{1000, 1000000, 1000000000};
+
+    int square{1};
+    int currentRiceonSquare{1};
+    int previousRiceonSquare{0};
     char rice_start;
     int squareStart;
+
+    for (int i = 0; i < riceGoals.size(); ++i)
+        {
+            while (riceGoals[i] > previousRiceonSquare + currentRiceonSquare)
+                {
+                    cout << "Square: " << square++ << endl;
+                    cout << " Grains on current square: " << currentRiceonSquare << endl;
+                    cout << " Rice on previous squares: " << previousRiceonSquare << endl;
+                    cout << " Total rice grans: " << previousRiceonSquare + currentRiceonSquare <<endl;
+
+                    previousRiceonSquare += currentRiceonSquare;
+                    currentRiceonSquare *= 2;
+
+                }
+        }
 
     cout << "Press s to start rice and square counter. " << endl;
             while (cin >> rice_start)
             {
-                 if ('s' == rice_start)
+                 if ('s' == rice_start);
 
 
-
-                while (riceAmount <= 1000000000 && squareNumb <= 64)
-                {
-                    cout << riceAmount << " grains of rice on square " <<  squareNumb << endl;
-                    squareNumb++;
-                    riceAmount *=2;
-
-
-
-                }
 
 
             }
