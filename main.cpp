a7bffd390d3657558007555cd7f8f3e72eed520c
Edwin Culpepper <ebculpepper246810@gmail.com>
2020-07-09 11:55:39 -0400

    I think this is what it was supposed to do. Im not sure if this is right

--- main.cpp
+++ main.cpp
@@ -10,37 +10,56 @@ using namespace std;
 int main()
 {
     vector<int> riceGoals{1000, 1000000, 1000000000};
-
+    int j;
     int square{1};
     int currentRiceonSquare{1};
     int previousRiceonSquare{0};
-    char rice_start;
-    int squareStart;
-
+    vector<int> squareGoal [3];
+    int riceTotal = previousRiceonSquare + currentRiceonSquare;
+    vector <int> squareReturn;
     for (int i = 0; i < riceGoals.size(); ++i)
         {
-            while (riceGoals[i] > previousRiceonSquare + currentRiceonSquare)
+
+            while (riceGoals[i] >= previousRiceonSquare + currentRiceonSquare)
                 {
-                    cout << "Square: " << square++ << endl;
+                    cout << " Square: " << square++ << endl;
                     cout << " Grains on current square: " << currentRiceonSquare << endl;
                     cout << " Rice on previous squares: " << previousRiceonSquare << endl;
-                    cout << " Total rice grans: " << previousRiceonSquare + currentRiceonSquare <<endl;
+                    cout << " Total rice grans: " << previousRiceonSquare + currentRiceonSquare << endl << endl;
 
                     previousRiceonSquare += currentRiceonSquare;
                     currentRiceonSquare *= 2;
+                    int riceTotal = previousRiceonSquare + currentRiceonSquare;
+
+
+                    if (riceTotal >= riceGoals[i])
+                    {
+                        squareReturn.push_back(square);
+                        i++;
+                    }
+                    if (riceTotal >= riceGoals[2] )
+                    {
+                        cout << "The amount of grains needed are on the respective squares " << "For 1000 grains youll have to be on " << squareReturn[0] << endl << endl;
+
+                         cout << "For 1000000 grains youll atleast have to be on square: "<< squareReturn[1] << endl << endl;
+
+                         cout<< "For 1000000000 grains of rice youll need to be on at least square: " << squareReturn[2]  << endl << endl;
+
+                         cout<< "Those are all the needed squares for reaching the different rice grain goals."  <<  squareReturn[3] << endl << endl;
+                        break;
+                    }
+
 
                 }
+
+
+
+
+
+
+
         }
 
-    cout << "Press s to start rice and square counter. " << endl;
-            while (cin >> rice_start)
-            {
-                 if ('s' == rice_start);
-
-
-
-
-            }
     return 0;
 }
 
