efd10290ed9d641467bd5ba466919f5334e4d65b
Edwin Culpepper <ebculpepper246810@gmail.com>
2020-07-14 09:35:42 -0400

    Quadratic Equation solver, still needs to be fixed.

--- main.cpp
+++ main.cpp
@@ -6,43 +6,67 @@
 #include <stdexcept>
 
 using namespace std;
+bool quadFormula(double varA , double varB, double varC, double *quadResult)
+{
+  double quadra = sqrt( (varB* varB) - (4*varA*varC) );
+
+  if (!varA)
+  {
+      cout << "UNAVAILABLE"\n;
+      return false;
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
 
 int main()
 {
+    double varA;
+    double varB;
+    double varC;
+    double [2]
+    cout << "The quadratic equation is ax^2 + bx + c = 0" << endl;
+    cout << "Enter the variables for a , b , and finally c followed by with 'Enter' after all the values have been given." << endl;
 
-    int number,i,j;
-    cout << "Enter the number" << endl;
-    cout << "NOTE: This does have a limit so dont put in number like 100000000000000000000000000000000." << endl;
-    cin >> number;
-
-    int primes[number+1];
+    cin >> varA >> varB >> varC;
 
 
-    for(i = 2; i<=number; i++)
-        primes[i] = i;
 
-    i = 2;
-    while ((i*i) <= number)
-    {
-        if (primes[i] != 0)
-        {
-            for(j=2; j<number; j++)
-            {
-                if (primes[i]*j > number)
-                    break;
-                else
-                    primes[primes[i]*j]=0;
-            }
-        }
-        i++;
-    }
 
-    for(i = 2; i<=number; i++)
-    {
 
-        if (primes[i]!=0)
-            cout << primes[i] << endl;
 
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
