312835dc8dd6142d7aaa163b47bd38bb6be0db88
Edwin Culpepper <ebculpepper246810@gmail.com>
2020-07-16 15:50:24 -0400

    Mostly done. The issue is it keeps outputting INVALID ROOT instead of answer like I thought I had.

--- main.cpp
+++ main.cpp
@@ -6,16 +6,6 @@
 #include <stdexcept>
 
 using namespace std;
-bool quadFormula(double varA , double varB, double varC, double *quadResult)
-{
-  double quadra = sqrt( (varB* varB) - (4*varA*varC) );
-
-  if (!varA)
-  {
-      cout << "UNAVAILABLE"\n;
-      return false;
-
-  }
 
 
 
@@ -23,50 +13,45 @@ bool quadFormula(double varA , double varB, double varC, double *quadResult)
 
 
 
-
-
-
-
-
-}
 
 
 
 
 int main()
 {
+    double x1;
+    double x2;
     double varA;
     double varB;
     double varC;
-    double [2]
+
     cout << "The quadratic equation is ax^2 + bx + c = 0" << endl;
-    cout << "Enter the variables for a , b , and finally c followed by with 'Enter' after all the values have been given." << endl;
+    cout << "Enter the variables for a , b , and finally c followed by with 'Enter' after each the value is given." << endl;
 
     cin >> varA >> varB >> varC;
 
+    if (varA == 0 && varB ==0)
+    	{
+    		cout << "I N V A L I D" << endl;
+    	}
+    if (varA == 0 && varB != 0)
+    	{
+    		x1 = -(varC/varB);
+    		cout << "root: " << x1 << endl;
 
+    	}
+    if ((varB*varB-4*varA*varC)>0)
+    	{
+    		x2=(varB*varB)-(4*varA*varC);
+    		x1=-varB+sqrt(x2);
+    		cout << "Root: "<< x1 << endl;
+    	}
+    if ((varB*varB-4*varA*varC)<0)
+    {
+    	cout << "I N V A L I D     R O O T" << endl;
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return 0;
 
 
 }
